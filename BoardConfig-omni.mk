c05503baf3b8c074e598c0148895aea8ee1f8329
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:25:36 +0530

    Build vendor
    Change-Id: I08dff42223ae54386a5ed050b51aa4a39e08bdb6

--- BoardConfig-omni.mk
+++ BoardConfig-omni.mk
@@ -1,6 +1,8 @@
 # Partitions
 AB_OTA_PARTITIONS += \
+    vendor \
     vendor_dlkm
 
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm#
